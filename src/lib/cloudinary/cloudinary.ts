c491c414277602a6e75df09e373994e3bf48b2c5
DJ5harma <sunubhargav2004@gmail.com>
2024-09-02 20:32:19 +0530

    there was a problem getting the public_cloudname for so many days cause I removed that from process.env

--- src/lib/cloudinary/cloudinary.ts
+++ src/lib/cloudinary/cloudinary.ts
@@ -1,7 +1,8 @@
 import { v2 as cloudinary } from "cloudinary";
+import { public_cloudname } from "../utils";
 
 cloudinary.config({
-	cloud_name: process.env.PUBLIC_CLOUDINARY_CLOUD_NAME,
+	cloud_name: public_cloudname,
 	api_key: process.env.CLOUDINARY_API_KEY,
 	api_secret: process.env.CLOUDINARY_API_SECRET,
 });
